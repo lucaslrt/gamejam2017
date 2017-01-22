56b2a3ee2de67b00ecd5aaf4c71b53c16887b26a
Lucas Rufino <Lucas Rufino>
2017-01-21 23:03:58 -0200

    initial commit

---
+++ gamejam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public PlayerController player;
+
+    private Vector3 lastPlayerPosition;
+    private float distanceToMove;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+        lastPlayerPosition = player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        distanceToMove = player.transform.position.x - lastPlayerPosition.x;
+
+        // The transform of the position of the object attached to this script
+        // The camera won't jump with the player
+        transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+
+        lastPlayerPosition = player.transform.position;
+	}
+}
---
+++ gamejam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D playerRigidbody;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D playerCollider;
+
+    private Animator playerAnimator;
+
+	// Use this for initialization
+	void Start () {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        grounded = Physics2D.IsTouchingLayers(playerCollider, whatIsGround);
+
+        playerRigidbody.velocity = new Vector2(moveSpeed, playerRigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            if(grounded)
+            {
+                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpForce);
+            }
+        }
+
+        playerAnimator.SetFloat("Speed", playerRigidbody.velocity.x);
+        playerAnimator.SetBool("Grounded", grounded);
+    }
+}
